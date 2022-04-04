5cca139f9f0a6d75844617f083d1e30de7276fd9
Larry James <larryjdel@gmail.com>
2022-04-04 14:57:18 -0700

    front page and back end
    front page edits
    backend add and read data

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 // Initialize Cloud Firestore through Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
-
+import { getFirestore, doc, getDoc, setDoc, collection } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
+//On deployment we write import { doc, getDoc, setDoc, collection } from "firebase/firestore"; 
 const firebaseConfig = initializeApp({
     apiKey: "AIzaSyC095wM6mpg04Sm8I_whR8h-3QXSe22-b4",
 
@@ -27,8 +27,27 @@ const db = getFirestore();
 
 //Here's an example of getting data from database
 async function getdata(db) {
+    const docRef = doc(db,"test","orders");
     const stuff = collection(db, 'test');
-    const stuffSnapshot = await getDocs(citiesCol);
-    const stuffList = citySnapshot.docs.map(doc => doc.data());
-    return stuffList;
+    const stuffSnapshot = await getDoc(docRef);
+    if (stuffSnapshot.exists()) {
+        console.log("Document data:", stuffSnapshot.data());
+        //const stuffList = stuffSnapshot.map(doc => doc.data());
+        return stuffSnapshot.data();
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+        return;
+      }
   }
+  getdata(db);
+
+  //Adding data
+  async function addData(db){
+  const dataToAdd = await setDoc(doc(db, "cities", "LA"), {
+    name: "Los Angeles",
+    state: "CA",
+    country: "USA"
+  });
+}
+  addData(db);
